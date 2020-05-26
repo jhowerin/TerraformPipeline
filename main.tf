12e8b23a43a4b448c0739f3f4cd92399664aa564
Jake <jhowerin@gmail.com>
2020-05-26 12:21:18 -0400

    May26 - adding main.tf

---
+++ main.tf
@@ -0,0 +1,20 @@
+provider "aws" {
+  profile    = "default"
+  region     = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-b374d5a5"
+  instance_type = "t2.micro"
+  name = "TheCloudCTO"
+}
+
+terraform {
+  backend "remote" {
+    organization = "TheCloudCTO"
+
+    workspaces {
+      name = "TheCloudCTO"
+    }
+  }
+}
